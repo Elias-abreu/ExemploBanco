9b5db2e7d6f7ad1ba9b47d78e7f0708742496572
Elias de Abreu <elias.silva@ifro.edu.br>
2023-08-28 06:54:14 -0400

    Definição da classe usuário

--- ExemploBanco/Entidades/Usuario.cs
+++ ExemploBanco/Entidades/Usuario.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace BancoIF.Entidades
 {
+    /*Classe responsável por gerenciamento de usuário*/
     internal class Usuario
     {
         public int Id_user { get; set; }
